0496aea8bdc6c197c61c2d63deb2d909b1ab31c0
MOON <uouon@naver.com>
2020-05-15 18:42:46 +0900

    Git 테스트

---
+++ src/main/java/com/example/demo/Hellow.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Hellow {
+
+	@RequestMapping("/")
+	public void hello( HttpServletResponse response ) throws IOException {
+
+		response.getWriter().print("test");
+	}
+}
